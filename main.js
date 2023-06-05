c121f010f12b1b11f1d1f4b82f7391fea9e46d94
l <109516388+Hamish2022@users.noreply.github.com>
2023-06-06 08:06:31 +1000

    main.js

--- main.js
+++ main.js
@@ -1,50 +1,44 @@
-    
-    let color = "black";
-    let blendValue = 1;
-    var size = 1;
+let color = "black";
+let blendValue = 1;
+var size = 1;
 
-    // Get the canvas element
-    const canvas = document.getElementById('myCanvas');
-    const context = canvas.getContext('2d');
+// Get the canvas element
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
 
-    let isDrawing = false;
+let isDrawing = false;
 
-    // Set up event listeners
-    canvas.addEventListener('mousedown', startDrawing);
-    canvas.addEventListener('mousemove', (event) => draw(event));
-    canvas.addEventListener('mouseup', stopDrawing);
-    canvas.addEventListener('mouseout', stopDrawing);
+// Set up event listeners
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', (event) => drawLine(event));
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
 
-    // Get the clear button element
-    const clearButton = document.getElementById('clearButton');
+// Get the clear button element
+const clearButton = document.getElementById('olo');
 
-    // Set up event listener for the clear button
-    olo.addEventListener('click', clearCanvas);
+// Set up event listener for the clear button
+clearButton.addEventListener('click', clearCanvas);
 
-    function startDrawing(event) {
-      isDrawing = true;
-      draw(event); // Pass the event object to the draw function
-    }
-    // Set up event listener for the change color button
+function startDrawing(event) {
+  isDrawing = true;
+  drawLine(event); // Pass the event object to the draw function
+}
+
+// Set up event listener for the change color button
 document.getElementById('change_color').addEventListener('click', setColor);
 
-
-// ...
-
-function draw(event) {
+function drawLine(event) {
   if (!isDrawing) return;
 
   const rect = canvas.getBoundingClientRect();
   const x = event.pageX - rect.left;
   const y = event.pageY - rect.top;
 
-  context.lineWidth = 5;
+  context.lineWidth = size;
   context.lineCap = 'round';
-
-  // Calculate the modified color with blending
-  const modifiedColor = applyBlending(color, blendValue);
-
-  context.strokeStyle = modifiedColor;
+  context.strokeStyle = color;
+  context.globalAlpha = blendValue; // Apply the blending value
 
   context.lineTo(x, y);
   context.stroke();
@@ -52,93 +46,49 @@ function draw(event) {
   context.moveTo(x, y);
 }
 
-// ...
-
-// ...
-
-// ...
-
-function applyBlending(color, blendValue) {
-  const parsedBlendValue = parseFloat(blendValue);
-  if (isNaN(parsedBlendValue)) return color;
-
-  const adjustedBlendValue = Math.max(Math.min(parsedBlendValue, 100), 1); // Ensure blendValue is between 1 and 100
-  const blendingFactor = adjustedBlendValue / 100; // Convert blendValue to a decimal between 0.01 and 1
-
-  const modifiedColor = blendColors(color, blendingFactor);
-
-  return modifiedColor;
+function stopDrawing() {
+  isDrawing = false;
+  context.beginPath();
 }
 
-
-
-
-
-
-function blendColors(color, blendingFactor) {
-  const r = parseInt(color.substr(1, 2), 16);
-  const g = parseInt(color.substr(3, 2), 16);
-  const b = parseInt(color.substr(5, 2), 16);
-
-  const blendedR = Math.round((1 - blendingFactor) * 255 + blendingFactor * r);
-  const blendedG = Math.round((1 - blendingFactor) * 255 + blendingFactor * g);
-  const blendedB = Math.round((1 - blendingFactor) * 255 + blendingFactor * b);
-
-  const blendedColor = rgbToHex(blendedR, blendedG, blendedB);
-
-  return blendedColor;
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
-
-function rgbToHex(r, g, b) {
-  const componentToHex = (c) => {
-    const hex = c.toString(16);
-    return hex.length === 1 ? "0" + hex : hex;
-  };
-
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+function setColor() {
+  color = document.getElementById("colorpicker").value;
+  size = document.getElementById("sizechoose").value;
+  blendValue = parseFloat(document.getElementById("blendValue").value); // Parse the value as a float
+  if (isNaN(blendValue)) blendValue = 1; // Use 1 as the default value if parsing fails
 }
 
-// ...
+function save() {
+  // Create a new canvas with white background
+  const tempCanvas = document.createElement('canvas');
+  const tempContext = tempCanvas.getContext('2d');
+  tempCanvas.width = canvas.width;
+  tempCanvas.height = canvas.height;
+  tempContext.fillStyle = 'white';
+  tempContext.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
+  
+  // Draw the original canvas onto the new canvas
+  tempContext.drawImage(canvas, 0, 0);
+  
+  // Create an image from the new canvas
+  const image = new Image();
+  image.src = tempCanvas.toDataURL();
 
+  // Create a download link
+  const link = document.createElement('a');
+  link.href = image.src;
+  link.download = 'canvas_image.png';
 
+  link.click();
+}
 
-// ...
-
-
-
-    function stopDrawing() {
-      isDrawing = false;
-      context.beginPath();
-    }
-
-    function clearCanvas() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function setColor() {
-      color = document.getElementById("colorpicker").value;
-      size = document.getElementById("sizechoose").value;
-      blendValue = parseFloat(document.getElementById("blendValue").value); // Parse the value as a float
-      if (isNaN(blendValue)) blendValue = 1; // Use 1 as the default value if parsing fails
-    }
-    
-    function draw(event) {
-      if (!isDrawing) return;
-    
-      const rect = canvas.getBoundingClientRect();
-      const x = event.pageX - rect.left;
-      const y = event.pageY - rect.top;
-    
-      context.lineWidth = size;
-      context.lineCap = 'round';
-      context.strokeStyle = color;
-      context.globalAlpha = blendValue; // Apply the blending value
-    
-      context.lineTo(x, y);
-      context.stroke();
-      context.beginPath();
-      context.moveTo(x, y);
-    }
+function erase()
+{
+  color = "white"
+  size = 5
+}
     
